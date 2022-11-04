cc4b7e532609fb9309d2699d575551d8fb913ce2
Dorota <dorotaostrowska@gmail.com>
2022-11-04 09:45:07 +0100

    poprawki

--- zadanie1.py
+++ zadanie1.py
@@ -7,8 +7,6 @@ Korzystając z pakietu NumPy podaj:
 - najwyższy oraz najniższy wzrost
 - 25 i 75 kwantyl
 - medianę.
-
-Wartości zaokrąglono do liczb całkowitych.
 """
 
 import numpy as np
@@ -17,17 +15,18 @@ import pandas as pd
 data = pd.read_csv('president_heights.csv')
 heights = np.array(data['height(cm)'])
 
-print("Średni wzrost: " + str(int(np.average(heights))))
+# Wartości zaokrąglono do liczb całkowitych
+print("Średni wzrost: {}".format(int(np.average(heights))))
 
-print("odchylenie standardowe: " + str(int(np.std(heights))))
+print("odchylenie standardowe: {}".format(int(np.std(heights))))
 
-print("najwyższy wzrost: " + str(int(np.max(heights))))
+print("najwyższy wzrost: {}".format(int(np.max(heights))))
 
-print("najniższy wzrost: " + str(int(np.min(heights))))
+print("najniższy wzrost: {}".format(int(np.min(heights))))
 
-print("25 kwantyl: " + str(int(np.quantile(heights, 0.25))))
+print("25 kwantyl: {}".format(int(np.quantile(heights, 0.25))))
 
-print("75 kwantyl: " + str(int(np.quantile(heights, 0.75))))
+print("75 kwantyl: {}".format(int(np.quantile(heights, 0.75))))
 
-print("mediana: " + str(int(np.median(heights))))
+print("mediana: {}".format(int(np.median(heights))))
 
